6377c83d4adcdc4565cb54fe90bb1d0bcdd15f0b
Eronic <eronicapp@gmail.com>
2026-02-08 03:00:55 -0500

    updated rates.js file

--- rate-service/api/rates.js
+++ rate-service/api/rates.js
@@ -1,12 +1,17 @@
 import axios from "axios";
-console.log("axios", axios);
+// console.log("axios", axios);
 
 export default async function handler(req, res){
     try{
         const response = await axios.get('https://api.frankfurter.dev/v1/latest?base=USD');
+        if(!response.ok){
+            throw new Error('frankfurter API responded with status: ${response.status}');
+        }
+        const data = await response.json();
         res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
         res.status(200).json(response.data);
     }catch(error){
-        res.status(500).json({error: 'Failed to fetch rates'});
+        res.status(500).json({error: 'Failed to fetch rates', details: error.message});
     }
 }
