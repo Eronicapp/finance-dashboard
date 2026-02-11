35221c08f2e5141d9259e490a5618d5617f67f34
Eronic <eronicapp@gmail.com>
2026-02-11 00:18:15 -0500

    Updated config file to reflect the url from server side

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 ﻿// Central config for API endpoints
 const CONFIG = {
   // API_URL: "https://api.frankfurter.app/latest?from=USD",
-  API_KEY_ENCODED: "aHR0cHM6Ly9hcGkuZnJhbmtmdXJ0ZXIuZGV2L3YxL2xhdGVzdD9iYXNlPVVTRA==",
+  API_KEY_ENCODED: "aHR0cHM6Ly9yYXRlLXNlcnZpY2UtYXNoaS52ZXJjZWwuYXBwLw==",
 };
